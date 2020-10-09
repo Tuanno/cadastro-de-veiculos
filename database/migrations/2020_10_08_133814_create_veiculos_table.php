c084824cca346c95ea8a0b9a20e1c176c1666b16
Tuanno <tuannodanyllo26@gmail.com>
2020-10-09 10:22:15 -0300

    Adicionando arquivos do projeto

---
+++ database/migrations/2020_10_08_133814_create_veiculos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVeiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('veiculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('marca');
+            $table->string('categoria');
+            $table->integer('ano');
+            $table->string('foto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('veiculos');
+    }
+}
---
+++ resources/views/create.blade.php
@@ -0,0 +1,63 @@
+<!-- create.blade.php -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">  
+    <script  data-src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>  
+  </head>
+  <body>
+    <div class="container">
+      <h2>Cadastro de veiculos</h2><br/>
+      <form method="post" action="{{url('veiculos')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="nome">Nome:</label>
+            <input type="text" class="form-control" name="nome">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="marca">Marca:</label>
+              <input type="text" class="form-control" name="marca">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="categoria">Categoria:</label>
+              <input type="text" class="form-control" name="categoria">
+            </div>
+          </div>
+           <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="ano">Ano:</label>
+              <input type="text" class="form-control" name="ano">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <input type="foto" name="foto">    
+         </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4" style="margin-top:60px">
+            <button type="submit" class="btn btn-success">Enviar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
